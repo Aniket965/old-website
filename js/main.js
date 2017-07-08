23354f7bfe00a957fa212a41990199d40cfabe44
aniket965 <aniket965.as@gmail.com>
2017-07-08 12:10:35 +0530

    added click !

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
+    $('.navigater').click(function() {
+        var clicked_id =  $(this).attr('id');
+        console.log('====================================');
+        console.log(clicked_id);
+        console.log('====================================');
+        $('.navigater').children().addClass('inactive');
+        $('.navigater').children().removeClass('active');
+        $('#'+clicked_id).children().removeClass('inactive');
+        $('#'+clicked_id).children().addClass('active');
+    });
 
+})
