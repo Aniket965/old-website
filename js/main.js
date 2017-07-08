e1e74d865d19b97f458650ef824db8419f034899
aniket965 <aniket965.as@gmail.com>
2017-07-08 13:19:08 +0530

    added links !

--- js/main.js
+++ js/main.js
@@ -1,13 +1,48 @@
 $(document).ready(function() {
+
+        initializeSocialLinks();
+        
     $('.navigater').click(function() {
         var clicked_id =  $(this).attr('id');
-        console.log('====================================');
-        console.log(clicked_id);
-        console.log('====================================');
+
         $('.navigater').children().addClass('inactive');
         $('.navigater').children().removeClass('active');
         $('#'+clicked_id).children().removeClass('inactive');
         $('#'+clicked_id).children().addClass('active');
-    });
+       
+       
+        // display social links only on home selected
+        if(clicked_id !=="home"){
+         $('.social').hide(300);
+        }
+        else{
+            $('.social').show(300);
+        }
+
+
+});
 
 })
+
+
+function initializeSocialLinks() {
+    $('.fa-facebook').click(function () {
+        window.open("https://www.facebook.com/aniket.sharma.583671", "facebook");
+    });
+    $('.fa-twitter').click(function () {
+        window.open("https://twitter.com/aniket965as", "twitter");
+    });
+    $('.fa-rss').click(function () {
+        window.open("http://aniket965.tech/blog", "blog");
+    });
+    $('.fa-linkedin').click(function () {
+        window.open("https://www.linkedin.com/in/aniket-sharma-548413131/", "linkedin");
+    });
+    $('.fa-medium').click(function () {
+        window.open("https://medium.com/@aniket965", "medium");
+    });
+    $('.fa-github').click(function () {
+        window.open("https://github.com/aniket965", "github");
+    });
+
+}
