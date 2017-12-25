1aca69f4a37075811267016a3284b12e3343e012
aniket965 <aniket965.as@gmail.com>
2017-12-25 20:20:04 +0530

    inverted💖💖

--- js/main.js
+++ js/main.js
@@ -1,94 +1,12 @@
-//TODO: Attach Projects
-//TODO: Add resume
-const ANDROID_COLOR_HEX = '#74C043',
-    WEB_COLOR_HEX = '#53B1E6',
-    BACKEND_COLOR_HEX = '#669F5B',
-    VR_COLOR_HEX = '#d89952'
 
 $(document).ready(function () {
     $('.projects').hide(0);
     $('.contacts').hide(0);
     $('.about-section').hide(0);
     initializeSocialLinks();
-    // Reveal Animations For Projects
-    $('#android-project').click(()=>{
-        var androidDiv = document.getElementById('android-project');
-        RevealAnimation(androidDiv,ANDROID_COLOR_HEX);
-    })
-    $('#web-project').click(()=>{
-        var webDiv = document.getElementById('web-project');
-        RevealAnimation(webDiv,WEB_COLOR_HEX)
-    })
-    $('#backend-project').click(()=>{
-        var webDiv = document.getElementById('backend-project');
-        RevealAnimation(webDiv,BACKEND_COLOR_HEX)
-    })
-    $('#vr-project').click(()=>{
-        var webDiv = document.getElementById('vr-project');
-        RevealAnimation(webDiv,VR_COLOR_HEX)
-    })
-    
-
-
-    $('.navigater').click(function () {
-        var clicked_id = $(this).attr('id');
-
-        $('.navigater').children().addClass('inactive');
-        $('.navigater').children().removeClass('active');
-        $('#' + clicked_id).children().removeClass('inactive');
-        $('#' + clicked_id).children().addClass('active');
-
-
-
-        if (clicked_id === "home") {
-            $('.contacts').hide(300);
-            $('.projects').hide(300);
-            $('.about-section').hide(300);
-            $('.social').show(300);
-        }
-        else if (clicked_id === "projects") {
-            $('.contacts').hide(300);
-            $('.social').hide(300);
-            $('.about-section').hide(300);
-            $('.projects').show(300);
-
-        }
-        else if (clicked_id === "contacts") {
-            $('.social').hide(300);
-            $('.projects').hide(300);
-            $('.about-section').hide(300);
-            $('.contacts').show(300);
-        }
-        else if (clicked_id === "about") {
-
-            $('.social').hide(300);
-            $('.projects').hide(300);
-            $('.contacts').hide(300);
-            $('.about-section').show(300);
-        }
-
-        else {
-            $('.social').hide(300);
-            $('.projects').hide(300);
-            $('.contacts').hide(300);
-            $('.about-section').hide(300);
-        }
-
-
-    });
-
 })
-function RevealAnimation(ele,hex) {
-    ele.style.backgroundColor = hex;
-    let scaleAnim =ele.animate([
-        {transform: 'scale(0)'},
-        {transform: 'scale(50)'},
-      ], {
-        duration: 450,
-        easing: 'cubic-bezier(.22,.67,.52,.92)',
-        fill: 'forwards',
-      });
-}
+
+
 
 function initializeSocialLinks() {
     $('.fa-facebook').click(function () {
